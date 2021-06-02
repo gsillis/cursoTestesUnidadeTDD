318861c21e9332e46c1d95954d6c7756a29b9908
Gabriela Sillis <gabydias47@gmail.com>
2021-06-02 16:31:48 -0300

    teste com mais de 5 lances do mesmo usuário criado

--- auctionAppTests/AuctionAppTests.swift
+++ auctionAppTests/AuctionAppTests.swift
@@ -10,15 +10,15 @@ import XCTest
 @testable import auctionApp
 
 class auctionAppTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
-       
+        
     }
-
+    
     override func tearDownWithError() throws {
         
     }
-
+    
     func testDeveReceberUmLance() {
         let leilao = Leilao(descricao: "Macbook Pro M1")
         XCTAssertEqual(0, leilao.lance?.count)
@@ -46,7 +46,7 @@ class auctionAppTests: XCTestCase {
         XCTAssertEqual(12.000, leilao.lance?[1].valor)
     }
     
-    func testDeveIgnorarOSegundoLanceDoMesmoUserNoMesmoProduto() {
+    func testDeveIgnorarDoisLancesSeguidosDoMesmoUsuario() {
         let leilao = Leilao(descricao: "Macbook Pro 2020 M1")
         let denner = Usuario(nome: "Denner")
         let gabriela = Usuario(nome: "Gabriela")
@@ -58,5 +58,32 @@ class auctionAppTests: XCTestCase {
         
         XCTAssertEqual(1, leilao.lance?.count)
         
+    }
+    
+    func testDeveIgnorarMaisDeCincoLancesDoMesmoUsuario() {
+        let leilao = Leilao.init(descricao: "Macbook Air m1")
+        let gabriela = Usuario.init(nome: "Gabriela")
+        let denner = Usuario.init(nome: "Denner")
+        
+        leilao.proposta(oferta: Lances(usuario: gabriela, valor: 3.000))
+        leilao.proposta(oferta: Lances(usuario: denner, valor: 4.000))
+        
+        leilao.proposta(oferta: Lances(usuario: gabriela, valor: 5.000))
+        leilao.proposta(oferta: Lances(usuario: denner, valor: 6.000))
+        
+        leilao.proposta(oferta: Lances(usuario: gabriela, valor: 7.000))
+        leilao.proposta(oferta: Lances(usuario: denner, valor: 8.000))
+        
+        leilao.proposta(oferta: Lances(usuario: gabriela, valor: 9.000))
+        leilao.proposta(oferta: Lances(usuario: denner, valor: 10.000))
+        
+        leilao.proposta(oferta: Lances(usuario: gabriela, valor: 11.000))
+        leilao.proposta(oferta: Lances(usuario: denner, valor: 12.000))
+        
+        //deve ignorar esta oferta
+        leilao.proposta(oferta: Lances(usuario: denner, valor: 15.000))
+        
+        XCTAssertEqual(10, leilao.lance?.count)
+        XCTAssertEqual(12.000, leilao.lance?.last?.valor)
     }
 }
